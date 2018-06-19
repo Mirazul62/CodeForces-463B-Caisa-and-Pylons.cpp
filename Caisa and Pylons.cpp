806f670967ca8b9c4fc4bf9fe59c3e2d63b7c4eb
Mirazul Islam <mirazkuetcse62@gmail.com>
2018-06-19 17:52:53 +0600

    463B	Caisa and Pylons solve in c++

---
+++ Caisa and Pylons.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n,i,a[100000],max=0;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        if(a[i]>max)
+            max=a[i];
+        else
+        {
+
+        }
+    }
+    cout<<max;
+}
